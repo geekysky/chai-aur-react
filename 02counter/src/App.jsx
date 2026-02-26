64271f04a8403cca86fae8475759613b72a79b43
geekysky <debu69980@gmail.com>
2026-02-26 16:49:34 +0530

    fix: improve counter functionality and alert messages in App component

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -6,25 +6,39 @@ import './App.css'
 function App() {
 
   let [counter, setCounter] = useState(20);
-  
+
   //let counter = 15;
 
-  const addValue = ()=>{
+  const addValue = () => {
     // counter++;
 
-    setCounter(counter++);
+    if (counter === 20) {
+      alert("Value has reached it's limit");
+    }
+    else {
+      setCounter(++counter);
 
-    console.log("Clicked",counter);
+      console.log("Clicked", counter);
+    }
+  };
+
+  const removeValue = ()=>{
+    if(counter===0){
+      alert("value cannot become negative");
+    }
+    else{
+      setCounter(--counter);
+    }
   }
 
   return (
-    <> 
+    <>
       <h1>Chai aur react</h1>
       <h2>Counter value:{counter}</h2>
 
       <button onClick={addValue}>Add value{counter}</button>
       <br></br>
-      <button>Remove value{counter}</button>
+      <button onClick={removeValue}>Remove value{counter}</button>
 
       <p>footer:{counter}</p>
     </>
